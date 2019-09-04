d18876598392b97ea72dcbadd41318cf06aab45f
Dima Korshakevich <dim.korsh2011@yandex.ru>
2019-09-04 20:04:24 +0300

    Code refactoring

--- bothandler.py
+++ bothandler.py
@@ -5,7 +5,34 @@ import pickle
 
 from msgstrings import *
 
-pickle_file = 'Queue bot.pkl'
+def first_match(iterable, predicate):
+    try:
+        return next(idx for idx,n in enumerate(iterable) if predicate(n))
+    except StopIteration:
+        return -1
+    
+def extract_first_word(message):
+    """ 
+    @param message: list of strings where each item is a paragraph in the message
+    @rtype: string
+    """
+    return message[0].split()[0].strip()
+
+def extract_args(message):
+    """ 
+    @param message: list of strings where each item is a paragraph in the message
+    @rtype: list of string args passed with the command
+    """
+    return message[0].split()[1:]
+
+def command_is(command, text):
+    """ 
+    @param command: a string containing the command to compare
+    @param text: a list of strings
+    @return: bool depending on whether the text is a given command
+    """
+    word_to_test = extract_first_word(text)
+    return word_to_test == command or word_to_test == command + '@exam_queue_bot' 
 
 class BotHandler:
     grouplists = {}
@@ -13,9 +40,7 @@ class BotHandler:
     def __init__(self, token):
         self.token = token
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
-        with open(pickle_file, 'rb') as f:
-            self.grouplists = pickle.load(f)
-            f.close()
+        self.load()
 
     def get_updates(self, offset=None, timeout=30):
         method = 'getUpdates'
@@ -26,6 +51,17 @@ class BotHandler:
         except KeyError:
             result_json = {}
         return result_json
+    
+    def get_last_update(self):
+        get_result = self.get_updates()
+
+        last_update = {}
+        if len(get_result) > 0:
+            last_update = get_result[-1]
+            last_update['ok'] = True
+        else:
+            last_update['ok'] = False
+        return last_update
 
     def send_message(self, text, chat_id, markdown = True):
         params = {'chat_id': chat_id, 'text': text}
@@ -43,42 +79,43 @@ class BotHandler:
             index += 1
         return result_txt
     
-    def get_last_update(self):
-        get_result = self.get_updates()
-
-        last_update = {}
-        if len(get_result) > 0:
-            last_update = get_result[-1]
-            last_update['ok'] = True
-        else:
-            last_update['ok'] = False
-        return last_update
-    
     def set_list(self, text, chat_id):
+        """
+        Saves the given list
+        @type text: list of str
+        @type chat_id: int
+        """
         self.grouplists[chat_id] = text
         self.save()
         
     def generate(self, chat_id):
+        """
+        Generates a new queue and forms the answer
+        @type chat_id: int
+        @return: string containing the answer of the bot
+        """
+        
         random.shuffle(self.grouplists[chat_id])
         self.save()
         return generate_success + self.list_to_txt(chat_id)
     
-    def get_numbers(self, indexes, chat_id):
+    def get_numbers(self, arguments, chat_id):
+        """
+        If there are people recorded with surnames, 
+        converts them into indexes in the list
+        
+        @type arguments: list of str
+        @type chat_id: int
+        
+        @return: list of int
+        """
         answer = list()
-        for index in indexes:
-            try:
-                answer.append(int(index) - 1)
-            except ValueError:
-                num = 0
-                added = False
-                for item in self.grouplists[chat_id]:
-                    if index.strip() == item.split()[0].strip():
-                        answer.append(num)
-                        added = True
-                        break
-                    num += 1
-                if not(added):
-                    answer.append(-1)
+        for arg in arguments:
+            if arg.isdigit():
+                answer.append(int(arg) - 1)
+            else:
+                answer.append(first_match(self.grouplists[chat_id], 
+                                          lambda x: x.split()[0] == arg))
         return answer
           
     def check_indexes(self, indexes, chat_id):
@@ -88,6 +125,15 @@ class BotHandler:
         return True
         
     def swap(self, indexes, chat_id):
+        """
+        Swaps persons under two given indexes
+        
+        @type indexes: list of int
+        @type chat_id: int
+        
+        @return: string containing the answer of the bot
+        """
+        
         if len(indexes) != 2:
             return swap_error
         
@@ -101,11 +147,18 @@ class BotHandler:
         return move_success + self.list_to_txt(chat_id)
     
     def move(self, indexes, chat_id):
+        """
+        Moves a person under the first index to the second index
+        
+        @type indexes: list of int
+        @type chat_id: int
+        
+        @return: string containing the answer of the bot
+        """
+        
         if len(indexes) != 2:
             return move_error
-        
         indexes = self.get_numbers(indexes, chat_id)
-        print(indexes)
         if not(self.check_indexes(indexes, chat_id)):
             return move_error
         
@@ -115,12 +168,53 @@ class BotHandler:
         cur_list.insert(indexes[1], cur_list[indexes[0]])
         if indexes[0] >= indexes[1]:
             indexes[0] += 1
-            
         cur_list.pop(indexes[0])
+        
         self.save()
         return move_success + self.list_to_txt(chat_id)
     
+    def process_command(self, message, chat_id):
+        """
+        A method that handles all available commands
+        
+        @param message: list of strings where each item is a paragraph in the message
+        @chat_id: int, the chat id from which the message came
+        
+        """
+        if command_is('/setlist', message):
+            if len(message) < 2:
+                self.send_message(list_creation_error, chat_id)
+            else:
+                self.send_message('Список сохранён.', chat_id)
+                self.set_list(message[1:], chat_id)
+            
+        if command_is('/generate', message):
+            self.send_message(self.generate(chat_id), 
+                                   chat_id)
+            
+        if command_is('/swap', message):
+            self.send_message(self.swap(extract_args(message), chat_id), 
+                              chat_id)
+            
+        if command_is('/move', message):
+            self.send_message(self.move(extract_args(message), chat_id), 
+                              chat_id)
+            
+        if command_is('/exception', message):
+            raise Exception('Test falling thrown by ' 
+                            + last_update['message']['from']['username'])
+    
+        if command_is('/show', message):
+            self.send_message(show_success + self.list_to_txt(chat_id), 
+                                   chat_id)
+    
+        if command_is('/info', message):
+            self.send_message(information, chat_id)
+        
     def save(self):
-        with open(pickle_file, 'wb') as f:
-            pickle.dump(self.grouplists, f)
-            f.close()
+        pass
+        # TODO
+        
+    def load(self):
+        pass
+        # TODO
--- script.py
+++ script.py
@@ -4,93 +4,54 @@ import random
 import pickle
 import sys
 
-from bothandler import BotHandler
+from bothandler import *
 from msgstrings import *
 
+# creator chat ID :)
 my_chat = 545584095
-now = datetime.datetime.now()
 
-commands_list = ['/generate', '/swap', '/move', '/exception', '/show']
-length = len(commands_list)
-for item in range(length):
+# list of commands that can't work if the grouplist is empty
+commands_list = ['/generate', '/swap', '/move', '/show']
+for item in range(len(commands_list)):
     commands_list.append(commands_list[item] + '@exam_queue_bot')
-
-def command_is(command, last_chat_text):
-    word_to_test = last_chat_text[0].split()[0].strip()
-    print('A', word_to_test)
-    return word_to_test == command or word_to_test == command + '@exam_queue_bot'    
-
+    
 def main():    
     new_offset = None
 
     while True:
-        if now.minute % 10 == 0:
-            greet_bot.save()
-            
-        greet_bot.get_updates(new_offset)
+        bot.get_updates(new_offset)
 
-        last_update = greet_bot.get_last_update()
+        # check for errors and damaged data
+        last_update = bot.get_last_update()
         if not(last_update['ok']):
             continue
 
         try:
             last_update_id = last_update['update_id']
-            last_chat_text = last_update['message']['text']
             last_chat_id = last_update['message']['chat']['id']
+            last_chat_text = last_update['message']['text']
+            last_chat_text = last_chat_text.split('\n')
         except KeyError:
             new_offset = last_update_id + 1
             continue
 
-        last_chat_text = last_chat_text.split('\n')
-        print(last_chat_text)
-        
-        if not last_chat_id in greet_bot.grouplists or not greet_bot.grouplists[last_chat_id]:
-            if last_chat_text[0].split()[0].strip() in commands_list:
-                greet_bot.send_message(empty_list, last_chat_id)
+        # check for attempts to work with an empty list
+        if not last_chat_id in bot.grouplists or not bot.grouplists[last_chat_id]:
+            if extract_first_word(last_chat_text) in commands_list:
+                bot.send_message(empty_list, last_chat_id)
                 new_offset = last_update_id + 1
                 continue
             
-        if command_is('/setlist', last_chat_text):
-            if len(last_chat_text) < 2:
-                greet_bot.send_message(list_creation_error, last_chat_id)
-            else:
-                greet_bot.send_message('Список сохранён.', last_chat_id)
-                greet_bot.set_list(last_chat_text[1:], last_chat_id)
-            
-        if command_is('/generate', last_chat_text):
-            greet_bot.send_message(greet_bot.generate(last_chat_id), 
-                                   last_chat_id)
-            
-        if command_is('/swap', last_chat_text):
-            greet_bot.send_message(greet_bot.swap(last_chat_text[0].split()[1:], 
-                                                  last_chat_id), last_chat_id)
-            
-        if command_is('/move', last_chat_text):
-            print(last_chat_text)
-            greet_bot.send_message(greet_bot.move(last_chat_text[0].split()[1:], 
-                                                  last_chat_id), last_chat_id)
-            
-        if command_is('/exception', last_chat_text):
-            raise Exception('Test falling thrown by ' 
-                            + last_update['message']['from']['username'])
-
-        if command_is('/show', last_chat_text):
-            greet_bot.send_message(show_success + greet_bot.list_to_txt(last_chat_id), 
-                                   last_chat_id)
-
-        if command_is('/info', last_chat_text):
-            greet_bot.send_message(information, last_chat_id)
+        bot.process_command(last_chat_text, last_chat_id)
             
         new_offset = last_update_id + 1
 
 if __name__ == '__main__':  
-    print(sys.argv[0])
     # a token for the bot is passed as an argument.
-    greet_bot = BotHandler(sys.argv[1])
+    bot = BotHandler(sys.argv[1])
     try:
         main()
     except Exception as e:
-        greet_bot.send_message(str(type(e)) + str(e), my_chat, markdown = False)
-        greet_bot.save()
-        greet_bot.send_message('Saved!', my_chat)
+        bot.send_message(str(type(e)) + str(e), my_chat, markdown = False)
+        bot.save()
         exit()
